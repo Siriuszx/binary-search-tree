aba360f50b0831e07a0fb1e9ae27b651e99301a6
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-11-08 20:50:34 +0100

    Add insertion and deletion WIP

--- Tree.js
+++ Tree.js
@@ -1,35 +1,93 @@
 import Node from "./Node.js";
 
 class Tree {
-    rootNode = null;
+    #rootNode = null;
+    #dataArray = null;
 
-    constructor(array) {
-        this.rootNode = this.arrayToBST(array);
+    constructor(array = []) {
+        this.#dataArray = this.removeDuplicatesSort(array);
+        this.#rootNode = this.buildTree();
     }
 
-    arrayToBST(dataArray) {
+    removeDuplicatesSort(array) {
+        return Object.values(array.reduce((accumulator, value) => {
+            if (!accumulator[`${value}`]) accumulator[`${value}`] = value;
+
+            return accumulator;
+        }, {}));
+    }
+
+    buildTree(dataArray = this.#dataArray) {
         if (dataArray.length == 0) return null;
 
         let midIndex = Math.floor(dataArray.length / 2);
 
         let midElement = dataArray[midIndex];
-        let leftNode = this.arrayToBST(dataArray.slice(0, midIndex));
-        let rightNode = this.arrayToBST(dataArray.slice(midIndex + 1));
+        let leftNode = this.buildTree(dataArray.slice(0, midIndex));
+        let rightNode = this.buildTree(dataArray.slice(midIndex + 1));
 
         let node = new Node(midElement, leftNode, rightNode);
 
         return node;
     }
 
-    prettyPrint(node = this.rootNode, prefix = "", isLeft = true) {
-        if (node === null) {
+    insert(value) {
+        this.#rootNode = this.#recInsert(this.#rootNode, value);
+    }
+
+    #recInsert(node, value) {
+        if (node == null) return new Node(value);
+
+        if (value < node.value)
+            node.nodeLeft = this.#recInsert(node.nodeLeft, value);
+        else if (value > node.value)
+            node.nodeRight = this.#recInsert(node.nodeRight, value);
+
+        return node;
+    }
+
+    delete(value) {
+        this.#rootNode = this.#recDelete(this.#rootNode, value);
+    }
+
+    #recDelete(node, value) {
+        if (node.value === value && !node.nodeLeft && !node.nodeRight) {
+            return null;
+        }
+        else if (node.value === value && node.nodeLeft && !node.nodeRight) {
+            node = node.nodeLeft;
+            return node;
+        }
+        else if (node.value === value && node.nodeRight && !node.nodeLeft) {
+            node = node.nodeRight;
+            return node;
+        } else if (node.value === value && node.nodeRight && node.nodeLeft) {
+            
+        }
+
+        if (value < node.value)
+            node.nodeLeft = this.#recDelete(node.nodeLeft, value);
+        else if (value > node.value)
+            node.nodeRight = this.#recDelete(node.nodeRight, value);
+
+        return node;
+    }
+
+    #recNextSmallest(nodeToDelete, parentNode) {
+        
+    }
+
+    prettyPrint(node = this.#rootNode, prefix = "", isLeft = true) {
+        if (node == null) {
             return;
         }
-        if (node.nodeRight !== null) {
+
+        if (node.nodeRight != null) {
             this.prettyPrint(node.nodeRight, `${prefix}${isLeft ? "│   " : "    "}`, false);
         }
+
         console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-        if (node.nodeLeft !== null) {
+        if (node.nodeLeft != null) {
             this.prettyPrint(node.nodeLeft, `${prefix}${isLeft ? "    " : "│   "}`, true);
         }
     }
--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 import Tree from "./Tree.js";
 
-const numArray = [1, 2, 3, 4, 5, 6, 7];
+const numArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-const tree = new Tree(numArray);
+const tree = new Tree();
+
+console.log("Before insert:");
 
 tree.prettyPrint();
 
+tree.insert(6);
+tree.insert(4);
+tree.insert(2);
+tree.insert(7);
+tree.insert(5);
 
+console.log("After insert:");
 
+tree.prettyPrint();
+
+console.log("After deletion:");
+
+tree.delete(4);
+tree.prettyPrint();
